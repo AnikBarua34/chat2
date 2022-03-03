bf2cd8ee56a0ac8cd114775555f571a2d566aec9
unknown <AnikBarua34>
2022-03-03 14:44:39 +0600

    heroku added

---
+++ src/components/Chatbox/Chat/Chat.js
@@ -0,0 +1,78 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line linebreak-style
+/* eslint-disable no-shadow */
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://anik-chat2.herokuapp.com/';
+
+let socket;
+
+// eslint-disable-next-line react/prop-types
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react/prop-types
+    // eslint-disable-next-line no-shadow
+    // eslint-disable-next-line react/prop-types
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name);
+
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
